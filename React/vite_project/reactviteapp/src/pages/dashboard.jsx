f8914a5bcf4a364da8e94c05bc68563272f10dee
prakhar <prakkhar.n03@gmail.com>
2025-11-26 10:25:46 +0530

    class work

--- React/vite_project/reactviteapp/src/pages/dashboard.jsx
+++ React/vite_project/reactviteapp/src/pages/dashboard.jsx
@@ -1,9 +1,40 @@
-import React from "react";
-function Dashboard() {
-    return (
+import React, { useEffect, useState } from 'react'
+
+const Dashboard = () => {
+//   const [count, setCount] = useState(0)
+//   const [point ,setPoint] = useState(10)
+//   useEffect(() => {
+//     console.log(count)
+//     console.log(point)
+//   },[count,point])
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const serverresponse = await fetch('https://fakestoreapi.com/products');
+      const jsonresponse = await serverresponse.json();
+      console.log(jsonresponse);
+      setData(jsonresponse);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data.length === 0 ? (
+        <h1>Data could not fetch</h1>
+      ) : (
         <div>
-            <h2>Welcome to Dashboard</h2>
+          {data.map((ele) => (
+            <div key={ele.id} style={{ border: "2px solid red" }}>
+              <img src={ele.image} alt="" height={100} width={100} />
+              <h2>{ele.title}</h2>
+            </div>
+          ))}
         </div>
-    )
+      )}
+    </div>
+  );
 }
+
 export default Dashboard
